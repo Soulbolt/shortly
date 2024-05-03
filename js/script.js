b2a4e8f4ef7d7ffc5396dff31df13b34c320d550
Soulbolt <soulbolt@gmail.com>
2024-05-03 11:57:07 -0700

    Add if statements to check when input is null, add function to validate Url and check if correct. Otherwise display error msg and remove msg + border if url is valid. Display alert with success msg.

--- js/script.js
+++ js/script.js
@@ -4,13 +4,32 @@ const errMsg = document.getElementById("err-msg");
 
 linkForm.addEventListener("submit", formSubmit);
 
+// Validate URL
+function valudateURL(url) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(url);
+}
+
 function formSubmit(e) {
   e.preventDefault();
 
   if (input.value === "") {
     errMsg.innerHTML = "Please add a link";
     input.classList.add("border-red");
+  } else if (!valudateURL(input.value)) {
+    errMsg.innerHTML = "Please add a valid url";
+    input.classList.add("border-red");
   } else {
     errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+    alert("Link shortened successfully!");
   }
 }
