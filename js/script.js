b6c2ea6d91498268b42c7c2eed703ec82dadf18c
Soulbolt <soulbolt@gmail.com>
2024-05-03 11:48:08 -0700

    Add js folder with script.js file and add consts for input, linkForm, and errMsg. Add listener for linkForm and Create a function on submit. When value is empty, display error msg and red border around input. Add errMG container.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === "") {
+    errMsg.innerHTML = "Please add a link";
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "";
+  }
+}
